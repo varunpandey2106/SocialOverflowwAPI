b1105562435c579944345e7c19d5a88399ee77a2
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-28 21:45:19 +0530

    user models timestampedmodel

--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid 
 
 # Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at= models.DateTimeField(auto_now_add=True) #profile create time
+    updated_at= models.DateTimeField(auto_now_add=True) #profile update time
+
+    class Meta:
+        abstract= True
+
+def user_directory_path(instance, filename):
+    return "users/{0}/{1}".format(instance.user.username,filename)
+
+AUTH_PROVIDERS = {'facebook': 'facebook', 'google': 'google',
+                  'twitter': 'twitter', 'email': 'email', 'github':'github'}
+
