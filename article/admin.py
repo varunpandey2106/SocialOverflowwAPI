ef2f8e252b8562f7b407e3ab7d6f631da4ab0f23
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-29 05:24:49 +0530

    article serializers added

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article, ArticleVote, ArticleComment
 
 # Register your models here.
+
+admin.site.register(Article)
+admin.site.register(ArticleVote)
+admin.site.register(ArticleComment)
---
+++ article/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from .models import Article, ArticleComment, ArticleVote
+
+from user.serializers import UserProfileSerializer, TopicTagSerializer
+
+class ArticleSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
+    tags = TopicTagSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+    def get_user(self, obj):
+        user = obj.user.userprofile
+        serializer = UserProfileSerializer(user, many=False)
+        return serializer.data
+
+class ArticleCommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = ArticleComment
+        fields = '__all__'
+
+    def get_user(self, obj):
+        user = obj.user.userprofile
+        serializer = UserProfileSerializer(user, many=False)
+        return serializer.data
+
+class ArticleVoteSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = ArticleVote
+        field = '__all__'
+    
+    def get_user(self, obj):
+        user = obj.user.userprofile
+        serializer = UserProfileSerializer(user, many=False)
+        return serializer.data
