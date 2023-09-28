c9770f9431a66174376905945a0fae63d365f5db
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-29 02:35:17 +0530

    reguster view

--- user/views.py
+++ user/views.py
@@ -46,3 +46,41 @@ def email_validator(email):
         return email_add
     except EmailNotValidError as e:
         return str(e)
+
+
+class RegisterView(APIView):
+    permission_classes = [permissions.AllowAny]
+    authentication_classes = []
+
+    def post(self, request):
+        data = request.data
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        email_valid_check_result = email_validator(email)
+        messages = {'errors':[]}
+        if username == None:
+            messages['errors'].append('username can\'t be empty')
+        if email == None:
+            messages['errors'].append('Email can\'t be empty')
+        if not email_valid_check_result == email:
+            messages['errors'].append(email_valid_check_result)
+        if password == None:
+            messages['errors'].append('Password can\'t be empty')
+        if User.objects.filter(email=email).exists():
+            messages['errors'].append("Account already exists with this email id.")    
+        if User.objects.filter(username__iexact=username).exists():
+            messages['errors'].append("Account already exists with this username.") 
+        if len(messages['errors']) > 0:
+            return Response({"detail":messages['errors']},status=status.HTTP_400_BAD_REQUEST)
+        try:
+            user = User.objects.create(
+                username=username,
+                email=email,
+                password=make_password(password)
+            )
+            serializer = UserSerializerWithToken(user, many=False)
+        except Exception as e:
+            print(e)
+            return Response({'detail':f'{e}'},status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data)
