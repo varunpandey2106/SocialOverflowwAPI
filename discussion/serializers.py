4bd887246da7c7edd1e2aadb1a3e191cca8510c1
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-29 23:11:24 +0530

    discussion serializers added

---
+++ discussion/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import Discussion, DiscussionVote, DiscussionComment
+from user.serializers import UserProfileSerializer, TopicTagSerializer
+
+class DiscussionSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
+    tags = TopicTagSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Discussion
+        fields = '__all__'
+
+    def get_user(self, obj):
+        user = obj.user.userprofile
+        serializer = UserProfileSerializer(user, many=False)
+        return serializer.data
+    
+class DiscussionCommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = DiscussionComment
+        fields = '__all__'
+
+    def get_user(self, obj):
+        user = obj.user.userprofile
+        serializer = UserProfileSerializer(user, many=False)
+        return serializer.data
+
+class DiscussionVoteSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = DiscussionVote
+        field = '__all__'
+    
+    def get_user(self, obj):
+        user = obj.user.userprofile
+        serializer = UserProfileSerializer(user, many=False)
+        return serializer.data
