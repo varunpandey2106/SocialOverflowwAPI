308db82d181d8acb227abac40874d150554dbab9
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-29 22:57:08 +0530

    discussion models

--- discussion/admin.py
+++ discussion/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Discussion, DiscussionComment, DiscussionVote
 
 # Register your models here.
+
+admin.site.register(Discussion)
+admin.site.register(DiscussionComment)
+admin.site.register(DiscussionVote)
--- discussion/models.py
+++ discussion/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from user.models import TopicTag
+import uuid
 
 # Create your models here.
+
+class Discussion(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
+    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+    headline = models.CharField(max_length=500, default="no headline")
+    content = RichTextField(max_length=10000)
+    # discussion tags from user model
+    tags = models.ManyToManyField(TopicTag, related_name='discussion_tags', blank=True) 
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.headline)
+
+
+class DiscussionComment(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
+    discussion = models.ForeignKey(Discussion,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+    content = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user.username)
+
+
+class DiscussionVote(models.Model):
+    id = models.UUIDField(default=uuid.uuid4,  unique=True, primary_key=True, editable=False)
+    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True, blank=True)
+    discussion = models.ForeignKey(Discussion, on_delete=models.CASCADE)
+    comment = models.ForeignKey(DiscussionComment, on_delete=models.SET_NULL,null=True, blank=True)
+    value = models.IntegerField(blank=True, null=True, default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.discussion} - count - {self.value}"
