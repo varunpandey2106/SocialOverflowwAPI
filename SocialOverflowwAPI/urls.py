5fc79a581c6b1299db2caa9e12e2a19381e20472
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-29 05:53:24 +0530

    article routing config

--- SocialOverflowwAPI/urls.py
+++ SocialOverflowwAPI/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls') )
+    path('user/', include('user.urls') ),
+    path('article/', include('article.urls'))
 
 ]
---
+++ article/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .import views 
+
+app_name= 'article'
+
+urlpatterns = [
+    path('', views.articles, name='article'), 
+    path('create/',views.create_article,name='create-article'),
+    path('vote/',views.update_vote,name='article-vote'),
+    path('<str:pk>/', views.get_article, name="get-article"),
+    path('edit/<str:pk>/', views.edit_article, name="edit-article"),
+    path('delete/<str:pk>/', views.delete_article, name="delete-article"),
+    path('edit-comment/<str:pk>/', views.edit_article_comment, name="edit-article-comment"),
+    path('delete-comment/<str:pk>/', views.delete_article_comment, name="delete-article-comment"),
+]
+
