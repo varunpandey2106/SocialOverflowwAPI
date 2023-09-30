d865c285cd4626f3c22c8954df0ad03a5e91b2b4
varunpandey2106 <varun.pandey2106@gmail.com>
2023-09-30 14:58:21 +0530

    Backend,twilio,celery config added

---
+++ SocialOverflowwAPI/celery.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SocialOverflowwAPI.settings.base")
+
+app = Celery("ecommerce", broker=settings.BROKER_URL, backend=settings.BACKEND_URL)
+
+app.config_from_object("django.conf:settings")
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
---
+++ notification/twilio.py
@@ -0,0 +1,24 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+account_sid = " "
+auth_token = " "
+verify_sid = " "
+verified_number = "+918355921551"
+
+client = Client(account_sid, auth_token)
+
+verification = client.verify.v2.services(verify_sid) \
+  .verifications \
+  .create(to=verified_number, channel="sms")
+print(verification.status)
+
+otp_code = input("Please enter the OTP:")
+
+verification_check = client.verify.v2.services(verify_sid) \
+  .verification_checks \
+  .create(to=verified_number, code=otp_code)
+print(verification_check.status)
